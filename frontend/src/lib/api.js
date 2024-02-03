c257163fc807a6579b3f6889df719d395297559f
alpapie <alpapie0908@gmail.com>
2024-02-03 20:20:53 +0000

    merg: merge pape ogin page in master

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -6,28 +6,14 @@ export async function makeRequest(endpoint, method, data = {}, headers = {}) {
     console.log(url)
   const config = { method, headers };
   if (method !== "GET") {
-    const formData = data;
-    // for (const [key, value] of Object.entries(data)) {
-    //   if (key === "file") {
-    //     formData.append(key, value, value.name);
-    //     config.headers["Content-Type"] = "multipart/form-data";
-    //   } else {
-    //     formData.append(key, value);
-    //   }
-    // }
-    // console.log(formData.get("email"))
-    config.data = formData;
+
+    config.data = data;
   }
-  // else {
-  //   const params = new URLSearchParams({ api_key: api_token, ...data });
-  //   url += `?${params.toString()}`;
-  // }
+  
   try {
     const response = await axios(url, config);
     return response;
   } catch (error) {
-    // console.error("Error occurred during request: ", error);
-    // redirect(301,'/error')
     return error
   }
 }
