3b7d48ff95fda5a47d67148704f633f639b97ea8
papgueye <ggueye294@gmail.com>
2024-02-05 13:09:07 +0000

    fix: check inputs on frontend when registered

--- frontend/src/routes/register/+page.server.js
+++ frontend/src/routes/register/+page.server.js
@@ -7,16 +7,57 @@ export const load =(event)=>{
     authenticateUser(event)
 }
 
+const validateFormData = (formData) => {
+    const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    const fields = ['email', 'password', 'firstname', 'lastname', 'birthdate'];
+
+    for (let field of fields) {
+        const value = formData.get(field);
+        if (!value || value.length < 2) {
+            return [false,field];
+        }
+        if (field === 'email' && !emailRegex.test(value)) {
+            return [false, field];
+        }
+    }
+
+    return [true,''];
+}
+
 export const actions = {
 	default: async ({request,}) => {
 		const formDatas= await request.formData()
         // data={
         //     email:formDatas.get()
         // }
-        console.log(formDatas);
-        let response= await makeRequest("register","POST",formDatas)
-        console.log(response)
-        // return response
+
+        // console.log(validateFormData(formDatas));
+        let errorMsg = '';
+        let [bool, err] = validateFormData(formDatas)
+        if (bool == true) {
+            let response = await makeRequest("register","POST",formDatas)
+            if (response.data.success == true) {
+                redirect(302, "/login")
+            }
+            errorMsg = response.data.error
+        }else{
+            errorMsg = err.toUpperCase() +' invalid. Veuillez verifier et reesayer.'
+        }
+        
+
+        let res = {
+            error: errorMsg,
+            email: formDatas.get('email'),
+            firstname: formDatas.get('firstname'),
+            lastname: formDatas.get('lastname'),
+            birth: formDatas.get('birthdate'),
+            nickname: formDatas.get('nickname'),
+            bio: formDatas.get('bio')
+            
+        }
+
+        return res;
+        
 	}
 };
 
