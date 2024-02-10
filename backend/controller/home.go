7354d21b4242e8214fcf94d0548ac044d9577978
jeeby_pndione <djibyndione26@gmail.com>
2024-02-10 16:39:06 +0000

    added the home handler

--- backend/controller/home.go
+++ backend/controller/home.go
@@ -1,8 +1,24 @@
 package controller
 
-import "net/http"
+import (
+	"net/http"
+	"social_network/helper"
+	"social_network/models"
+)
 
-func Home(w http.ResponseWriter, r *http.Request){
-	w.WriteHeader(500)
-	w.Write([]byte(`{"error":"Wrong methode","success":false}`))
+func Home(w http.ResponseWriter, r *http.Request) {
+	_, _, user_id := helper.Auth(DB, r)
+	user := models.User{ID: user_id}
+	listusers, err := user.GetFollow(DB, 4)
+
+	posts, errpost := user.GetPosts(DB)
+	if err != nil && errpost != nil {
+		helper.ErrorPage(w, 500)
+		return
+	}
+	err = helper.WriteJSON(w, http.StatusOK, map[string]interface{}{"success": true, "listesusers": listusers, "posts": posts}, nil)
+	if err != nil {
+		helper.ErrorPage(w, 400)
+		return
+	}
 }
---
+++ backend/models/follow.go
@@ -0,0 +1,31 @@
+package models
+
+import (
+	"database/sql"
+	"errors"
+)
+
+func (u *User) GetFollow(DB *sql.DB, limit int) ([]User, error) {
+	req := `select id,firstName,lastName,email,nickName from User `
+	row, err := DB.Query(req)
+	users := []User{}
+	if err != nil {
+		return users, err
+	}
+	for row.Next() {
+		row.Scan(&u.ID, &u.FirstName, &u.LastName, &u.Email, &u.NickName)
+		users = append(users, *u)
+	}
+	defer row.Close()
+	return users, nil
+}
+
+func (U *User) Folower(DB *sql.DB) error {
+	// req:=` select`
+	return errors.New("asas")
+}
+
+func (U *User) Following(DB *sql.DB) error {
+	// req:=` select`
+	return errors.New("asas")
+}
